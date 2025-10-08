753b87e7baad3c3199ba601ea3e14ef84bb18ac7
MinseoKim09 <u01056305988@gmail.com>
2025-10-08 21:41:25 +0900

    [Bronze III] Title: ACM 호텔, Time: 200 ms, Memory: 18096 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 0; i < T; i++) {
+            int H = sc.nextInt();
+            int W = sc.nextInt();
+            int N = sc.nextInt();
+            int floor = (N - 1) % H + 1;
+            int room = (N - 1) / H + 1;
+            int roomNumber = floor * 100 + room;
+            System.out.println(roomNumber);
+        }
+        sc.close();
+    }
+}
