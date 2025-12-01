69eee94f26b6a4dbaa779b7c9294fd4c08b5e051
Kim-Minseo <u01056305988@gmail.com>
2025-12-01 17:29:43 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 89.73 ms, Memory: 33.1 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,10 +1,5 @@
-def solution(p, c):
-    hmap = {}
-    th = 0
-    for x in p:
-        hx = hash(x)
-        th += hx
-        hmap[hx] = x
-    for y in c:
-        th -= hash(y)
-    return hmap[th]
+def solution(p, c): 
+    d = {}
+    [d.update({x: d.get(x,0)+1}) for x in p]
+    [d.update({x: d.get(x,0)-1}) for x in c]
+    return next(k for k,v in d.items() if v>0)
