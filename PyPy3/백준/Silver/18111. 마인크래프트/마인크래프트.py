948563d7196a1f4483b415dfac42c9ec922cd423
Kim-Minseo <u01056305988@gmail.com>
2025-12-22 18:57:56 +0900

    [Silver II] Title: 마인크래프트, Time: 120 ms, Memory: 110904 KB -BaekjoonHub

--- PyPy3/백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ PyPy3/백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,27 +1,27 @@
 import sys
 input = sys.stdin.readline
-N, M, B = map(int, input().split())
-land = []
+n, m, b = map(int, input().split())
+cnt = [0] * 257
 min_h = 256
 max_h = 0
-for _ in range(N):
-    row = list(map(int, input().split()))
-    land.append(row)
-    min_h = min(min_h, min(row))
-    max_h = max(max_h, max(row))
+for _ in range(n):
+    for h in map(int, input().split()):
+        cnt[h] += 1
+        min_h = min(min_h, h)
+        max_h = max(max_h, h)
 best_time = float('inf')
 best_height = 0
 for h in range(min_h, max_h + 1):
     remove = 0
     add = 0
-    for i in range(N):
-        for j in range(M):
-            diff = land[i][j] - h
-            if diff > 0:
-                remove += diff
-            else:
-                add -= diff
-    if remove + B < add:
+    for height in range(257):
+        if cnt[height] == 0:
+            continue
+        if height > h:
+            remove += (height - h) * cnt[height]
+        elif height < h:
+            add += (h - height) * cnt[height]
+    if remove + b < add:
         continue
     time = remove * 2 + add
     if time < best_time or (time == best_time and h > best_height):
