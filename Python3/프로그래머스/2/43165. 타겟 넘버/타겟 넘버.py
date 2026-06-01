16bf40109dfb7983ae827ab74046160165054735
Kim-Minseo <u01056305988@gmail.com>
2026-06-01 19:51:00 +0900

    [level 2] Title: 타겟 넘버, Time: 114.08 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,12 @@
+def solution(numbers, target):
+
+    def dfs(idx, total):
+        if idx == len(numbers):
+            return 1 if total == target else 0
+
+        return (
+            dfs(idx + 1, total + numbers[idx]) +
+            dfs(idx + 1, total - numbers[idx])
+        )
+
+    return dfs(0, 0)
